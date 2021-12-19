4939baaba7546b299673fb74dc6f462e0ea5183b
akash-starvin <Akashstarvin999@gmail.com>
2021-12-19 13:56:20 +0530

    Implemented category banner

--- src/app/product/listing-container/listing-container.component.ts
+++ src/app/product/listing-container/listing-container.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-listing-container',
   templateUrl: './listing-container.component.html',
-  styleUrls: ['./listing-container.component.scss']
+  styleUrls: ['./listing-container.component.scss'],
 })
 export class ListingContainerComponent implements OnInit {
+  path: string = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route?.parent?.url.subscribe((url) => {
+      this.path = url[0].path;
+    });
   }
-
 }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ListingContainerComponent } from './listing-container/listing-container.component';
+import { CategoryBannerComponent } from './category-banner/category-banner.component';
 
 
 @NgModule({
   declarations: [
-    ListingContainerComponent
+    ListingContainerComponent,
+    CategoryBannerComponent
   ],
   imports: [
     CommonModule,
