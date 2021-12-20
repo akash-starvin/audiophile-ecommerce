9e9c481182dceb8051f23bd5aa5486202e2b512c
akash-starvin <Akashstarvin999@gmail.com>
2021-12-20 15:46:41 +0530

    Implemented row reverse for alternating rows

--- src/app/product/components/product-card/product-card.component.ts
+++ src/app/product/components/product-card/product-card.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductCardComponent implements OnInit {
 
   @Input() data: any;
+  @Input() rowReverse: any;
 
   constructor() { }
 
