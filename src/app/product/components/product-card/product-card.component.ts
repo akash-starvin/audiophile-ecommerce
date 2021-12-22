4a9186c871b721cc887d3a29c9919ca04441cf89
akash-starvin <Akashstarvin999@gmail.com>
2021-12-22 18:55:30 +0530

    Implemented add to cart functionality using local storage

--- src/app/product/components/product-card/product-card.component.ts
+++ src/app/product/components/product-card/product-card.component.ts
@@ -1,41 +1,80 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Constants } from 'src/app/core/constants/Constants';
+import { Cart } from '../../interface/cart';
+import { Product } from '../../interface/product';
 import { ProductDataService } from '../../service/product-data.service';
 
 @Component({
   selector: 'product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
-
   @Input() data: any;
   @Input() rowReverse: any;
   @Input() isDetailView: any;
 
   MAX_COUNT: number = 10;
-  productCount: number = 0;
+  productCount: number = 1;
+  cartItems: Cart[] = [];
 
-  constructor(private productDataService: ProductDataService) { }
+  constructor(private productDataService: ProductDataService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  updateSelectedProduct(){
+  updateSelectedProduct() {
     this.productDataService.selectedProduct.next(this.data);
   }
 
   updateCount(val: number) {
     switch (val) {
       case -1:
-        if (this.productCount > 0) this.productCount--;
-
+        if (this.productCount > 1) this.productCount--;
         break;
       case 1:
         if (this.productCount < this.MAX_COUNT) this.productCount++;
-
         break;
-      case 0:
-        this.productCount = 0;
     }
+  }
+
+  checkProductExistsInLocal() {
+    //Remove if item exists in array
+    this.cartItems = this.cartItems.filter((item) => {
+      return item.id !== this.data.id;
+    });
+    let cartItemObject = this.createCartItemObject(this.data);
+    this.saveToLocalStorage(cartItemObject);
+  }
+
+  saveToLocalStorage(cartItem: Cart) {
+    this.cartItems.unshift(cartItem);
+    localStorage.setItem(
+      Constants.LOCAL_STORAGE_KEY,
+      JSON.stringify(this.cartItems)
+    );
+  }
+
+  createCartItemObject(product: Product): Cart {
+    return {
+      id: product.id,
+      image: product.image.mobile,
+      name: product.name,
+      price: product.price,
+      quantity: this.productCount,
+    };
+  }
+
+  getLocalCartItems() {
+    if (
+      localStorage.getItem(Constants.LOCAL_STORAGE_KEY) === '[]' ||
+      localStorage.getItem(Constants.LOCAL_STORAGE_KEY) === null
+    ) {
+      this.cartItems = [];
+    } else {
+      this.cartItems = JSON.parse(
+        localStorage.getItem(Constants.LOCAL_STORAGE_KEY) || '{}'
+      );
+    }
+    this.checkProductExistsInLocal();
   }
 }
---
+++ src/app/product/interface/cart.ts
@@ -0,0 +1,7 @@
+export interface Cart {
+  id: number;
+  image: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
